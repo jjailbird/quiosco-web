724cee51db9c984df727d7316c713406bd5ee172
cgpvibe <cgpvibe@gmail.com>
2025-05-26 19:18:03 +0900

    페이지 제목을 'quiosco.'에서 'quiosco.WEB'으로 변경했습니다.

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
       <div className='absolute inset-0 bg-black/70'></div>
       <div className='relative container mx-auto bg-white/0 flex justify-center items-center min-h-screen z-10'>
         <div className='text-4xl uppercase items-center flex flex-col font-light'>
-          <div>quiosco.</div>
+          <div>quiosco.WEB</div>
           <div>
             Meet a New Experience
           </div>
