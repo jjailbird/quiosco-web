057fff690eb893b9d8f20a6023f9b506db1bec3f
cgpvibe <cgpvibe@gmail.com>
2025-08-06 22:40:01 +0900

    CopyInfo.js 파일의 스타일을 수정하여 반응형 디자인을 개선하고, 배경색 및 패딩을 추가했습니다.

--- src/components/CopyInfo.js
+++ src/components/CopyInfo.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function CopyInfo() {
   return (
-    <div className='fixed bottom-16 left-16 space-y-1 z-20'>
+    <div className='relative xl:fixed xl:bottom-16 xl:left-16 text-white space-y-1 xl:z-20 bg-black xl:bg-transparent p-5 xl:p-0 w-full xl:w-auto'>
       <div className='uppercase'>
         &copy; 2025 quiosco.
       </div>
